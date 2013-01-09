b681ab45fcd7dfd16431d29e544211a227c99267
Muaz Khan <muazkh@gmail.com>
2013-01-09 14:25:38 +0500

    Realtime Chat using RTCDataChannel APIs
    A new experiment is added: Realtime Chat using RTCDataChannel APIs
    
    https://webrtc-experiment.appspot.com/chat/

---
+++ chat/2-rtc-functions.js
@@ -0,0 +1,59 @@
+﻿/* send (i.e. transmit) offer/answer sdp */
+function sendsdp(sdp, socket, isopus) {
+    sdp = JSON.stringify(sdp);
+    window.sdp = sdp;
+
+    var part = parseInt(sdp.length / 3);
+
+    /* because sdp size is larger than what pubnub supports for single request...that's why it is splitted in three parts */
+    var firstPart = sdp.slice(0, part),
+        secondPart = sdp.slice(part + 1, sdp.length - 1),
+        thirdPart = '';
+
+    if (sdp.length > part + part) {
+        secondPart = sdp.slice(part, part + part);
+        thirdPart = sdp.slice(part + part + 1, sdp.length);
+    }
+
+    /* transmitting first sdp part */
+    socket.send({
+        userToken: global.userToken,
+        firstPart: firstPart,
+
+        /* let other end know that whether you support opus */
+        isopus: isopus
+    });
+
+    /* transmitting second sdp part */
+    socket.send({
+        userToken: global.userToken,
+        secondPart: secondPart,
+
+        /* let other end know that whether you support opus */
+        isopus: isopus
+    });
+
+    /* transmitting third sdp part */
+    socket.send({
+        userToken: global.userToken,
+        thirdPart: thirdPart,
+
+        /* let other end know that whether you support opus */
+        isopus: isopus
+    });
+}
+
+/* send (i.e. transmit) ICE candidates */
+function sendice(candidate, socket) {
+    socket.send({
+        userToken: global.userToken, /* unique ID to identify the sender */
+        candidate: {
+            sdpMLineIndex: candidate.sdpMLineIndex,
+            candidate: JSON.stringify(candidate.candidate)
+        }
+    });
+}
+
+function gotstream() {
+    global.isGotRemoteStream = true;
+}
---
+++ chat/README.md
@@ -0,0 +1,24 @@
+![WebRTC Experiment!](https://muazkh.appspot.com/images/WebRTC.png)
+
+--
+
+A realtime chat app uses WebRTC DataChannel APIs to transmit text message over many peers!
+
+[This webrtc experiment](https://webrtc-experiment.appspot.com/chat/) uses socket.io as signaling gateway.
+
+Pubnub is used as a wrapper for socket.io
+
+[RTCPeerConnection-v1.2.js](https://webrtc-experiment.appspot.com/lib/RTCPeerConnection-v1.2.js) is used as JavaScript-Wrapper for RTCWeb APIs.
+
+You can share you text message with unlimited people!
+
+Don't forget to test it yourself!
+
+[https://webrtc-experiment.appspot.com/chat/](https://webrtc-experiment.appspot.com/chat/)
+
+##Credits
+
+* [Muaz Khan](http://github.com/muaz-khan)!
+
+## License
+Copyright (c) 2013 [Muaz Khan](https://plus.google.com/100325991024054712503) - Licensed under the MIT license.
