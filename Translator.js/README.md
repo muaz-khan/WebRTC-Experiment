2e441aeb04a5389f80816bad4d6fc10fc53961b1
Muaz Khan <muazkh@gmail.com>
2014-02-01 14:48:03 +0500

    Translator.js added. A library for Voice & Text Translation!
    Translator.js is a JavaScript library built top on Google
    Speech-Recognition & Translation API to transcript and translate voice
    and text.
    
    https://github.com/muaz-khan/WebRTC-Experiment/tree/master/Translator.js
    
    https://www.webrtc-experiment.com/Translator/

---
+++ Translator.js/README.md
@@ -0,0 +1,136 @@
+## [Translator.js](https://github.com/muaz-khan/WebRTC-Experiment/tree/master/Translator.js) / Voice & Text Translator / [Demo](https://www.webrtc-experiment.com/Translator/)
+
+Translator.js is a JavaScript library built top on Google Speech-Recognition & Translation API to transcript and translate voice and text. It supports many locales and brings globalization in <a href="https://www.webrtc-experiment.com/">WebRTC</a>!
+
+=
+
+##### How to use?
+
+```html
+<script src="//www.webrtc-experiment.com/Translator.js"> </script>
+```
+
+=
+
+##### Constructor
+
+```javascript
+var translator = new Translator();
+```
+
+=
+
+##### `voiceToText`
+
+This method allows you convert voice to text; whatever you speak is recognized using Google Speech-Recognition API; and converted into text using same API:
+
+```javascript
+translator.voiceToText(function (text) {
+    console.log('Your voice as text!', text);
+}, 'en-US');
+```
+
+`voiceToText` method takes two arguments:
+
+1. Callback; used to return recognized text
+2. Language; used to suggest Speech-Recognition API to easily recognize speaker's language
+
+If `language` is not passed; then `en-US` will be used as default language.
+
+You can use socket.io, websockets or any other signaling gateway like [WebRTC](https://www.webrtc-experiment.com/) data channels to exchange/share transcripted text with other users.
+
+=
+
+##### `translateLanguage`
+
+This method allows you convert language. You just need to pass text and get translated text through callback parameter:
+
+```javascript
+translator.translateLanguage(textToConvert, {
+    from: 'language-of-the-text',
+    to: 'convert-into',
+    callback: function (translatedText) {
+        console.log('translated text', translatedText);
+    }
+});
+```
+
+`translateLanguage` method takes two arguments:
+
+1. Text to convert
+2. Source and Target Languages; also callback method
+
+Second argument is an object.
+
+What you can do is either display translated text in chat-box; or use `speakTextUsingRobot` or `speakTextUsingGoogleSpeaker` methods to play voice.
+
+=
+
+##### `speakTextUsingRobot`
+
+This method uses meSpeak.js library to play text using a robot voice. Behind the scene; text is buffered and converted into WAV file; which is played using invisible `<audio>` element.
+
+```javascript
+translator.speakTextUsingRobot(textToPlay, {
+    workerPath: 'https://www.webrtc-experiment.com/Robot-Speaker.js',
+    callback: function (WAV_File) {},
+    amplitude: 100,
+    wordgap: 0,
+    pitch: 50,
+    speed: 175,
+    onSpeakingEnd: function() {}
+});
+```
+
+`speakTextUsingRobot` method accepts two arguments; first one is mandatory and last one is optional:
+
+1. Text to Speak i.e. convert text into voice file (WAV)
+2. Options like worker-file path etc.
+
+Default worker file's path is `https://www.webrtc-experiment.com/Robot-Speaker.js`. It is strongly recommended to download and link it from your own domain.
+
+If you want to play WAV file yourself or you want to POST/store WAV file; then you can use `callback` parameter to override default behaviour:
+
+```javascript
+translator.speakTextUsingRobot(textToPlay, {
+    callback: function (WAV_File) {
+        HTTP_POST_using_FormData( WAV_File );
+    }
+});
+```
+
+`onSpeakingEnd` is useful in text-chat apps; where you can disable text box until text is translated and spoken; then you can enable it again.
+
+=
+
+##### `speakTextUsingGoogleSpeaker`
+
+This method uses Google's Non-Official Translation API to convert text into mp3 sound. API Key used is taken from someone on the web; and there is no guarantee of its availability; that's why it is strongly suggested to buy your own KEY and pass using `api_key` parameter:
+
+```javascript
+translator.speakTextUsingGoogleSpeaker({
+    textToSpeak: 'text-to-convert',
+    targetLanguage: 'your-language',
+
+    // Google Translation service's API Key
+    api_key: 'Your-Private-API-Key'
+});
+```
+
+Both `textToSpeak` and `targetLanguage` are mandatory. Only `api_key` is optional.
+
+=
+
+##### Use Cases
+
+1. You can use it in any [WebRTC](https://www.webrtc-experiment.com/) application to support globalization!
+2. You can use WebRTC data channels to share transcripted text among users
+3. You can mute original voice; and play translated one. Though, voice and video will NOT be sync
+
+It is really useful in text-chat apps!
+
+=
+
+##### License
+
+[Translator.js](https://github.com/muaz-khan/WebRTC-Experiment/tree/master/Translator.js) is released under [MIT licence](https://www.webrtc-experiment.com/licence/) . Copyright (c) [Muaz Khan](https://plus.google.com/+MuazKhan).
---
+++ Translator.js/Translator.js
@@ -0,0 +1,168 @@
+// Last time updated at 01 Feb 2014, 05:46:23
+
+// Muaz Khan      - www.MuazKhan.com
+// MIT License    - www.WebRTC-Experiment.com/licence
+
+// Source Code    - github.com/muaz-khan/WebRTC-Experiment/tree/master/Translator.js
+// Demo           - www.webrtc-experiment.com/Translator
+
+function Translator() {
+    this.voiceToText = function(callback, language) {
+        initTranscript(callback, language);
+    };
+
+    this.speakTextUsingRobot = function(text, args) {
+        args = args || { };
+
+        if (!args.amplitude) args.amplitude = 100;
+        if (!args.wordgap) args.wordgap = 0;
+        if (!args.pitch) args.pitch = 50;
+        if (!args.speed) args.speed = 175;
+
+        // args.workerPath
+        // args.callback
+
+        Speaker.Speak(text, args);
+    };
+
+    this.speakTextUsingGoogleSpeaker = function(args) {
+        var textToSpeak = args.textToSpeak;
+        var targetLanguage = args.targetLanguage;
+
+        textToSpeak = textToSpeak.replace( /%20| /g , '+');
+        if (textToSpeak.substr(0, 1) == ' ' || textToSpeak.substr(0, 1) == '+') {
+            textToSpeak = textToSpeak.substr(1, textToSpeak.length - 1);
+        }
+
+        var audio_url = '//translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=' + textToSpeak.length + '&tl=' + targetLanguage + '&q=' + textToSpeak;
+
+        if (args.callback) args.callback(audio_url);
+        else {
+            var audio = document.createElement('audio');
+            audio.src = audio_url;
+            audio.autoplay = true;
+            audio.play();
+        }
+    };
+
+    this.translateLanguage = function(text, config) {
+        config = config || { };
+        // please use your own API key; if possible
+        var api_key = config.api_key || 'AIzaSyCUmCjvKRb-kOYrnoL2xaXb8I-_JJeKpf0';
+
+        var newScript = document.createElement('script');
+        newScript.type = 'text/javascript';
+
+        var sourceText = encodeURIComponent(text); // escape
+
+        var randomNumber = 'method' + (Math.random() * new Date().getTime()).toString(36).replace( /\./g , '');
+        window[randomNumber] = function(response) {
+            if (response.data && response.data.translations[0] && config.callback) {
+                config.callback(response.data.translations[0].translatedText);
+            }
+        };
+
+        var source = '//www.googleapis.com/language/translate/v2?key=' + api_key + '&target=' + (config.to || 'en-US') + '&callback=window.' + randomNumber + '&q=' + sourceText;
+        newScript.src = source;
+        document.getElementsByTagName('head')[0].appendChild(newScript);
+    };
+
+    var recognition;
+
+    function initTranscript(callback, language) {
+        if (recognition) recognition.stop();
+
+        window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+        recognition = new SpeechRecognition();
+
+        recognition.lang = language || 'en-US';
+
+        console.log('SpeechRecognition Language', recognition.lang);
+
+        recognition.continuous = true;
+        recognition.interimResults = true;
+
+        recognition.onresult = function(event) {
+            for (var i = event.resultIndex; i < event.results.length; ++i) {
+                if (event.results[i].isFinal) {
+                    callback(event.results[i][0].transcript);
+                }
+            }
+        };
+
+        recognition.onend = function() {
+            initTranscript(callback, language);
+        };
+
+        recognition.onerror = function(e) {
+            console.error(e);
+        };
+
+        recognition.start();
+    }
+
+    var Speaker = {
+        Speak: function(text, args) {
+            var callback = args.callback;
+            var onSpeakingEnd = args.onSpeakingEnd;
+
+            function processInWebWorker() {
+                var blob = URL.createObjectURL(new Blob(['importScripts("' + (args.workerPath || '//www.webrtc-experiment.com/Robot-Speaker.js') + '");this.onmessage =  function (event) {postMessage(generateSpeech(event.data.text, event.data.args));}'], {
+                    type: 'application/javascript'
+                }));
+
+                var worker = new Worker(blob);
+                URL.revokeObjectURL(blob);
+                return worker;
+            }
+
+            var speakWorker = processInWebWorker();
+            speakWorker.onmessage = function(event) {
+
+                function encode64(data) {
+                    var BASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+                    var PAD = '=';
+                    var ret = '';
+                    var leftchar = 0;
+                    var leftbits = 0;
+                    for (var i = 0; i < data.length; i++) {
+                        leftchar = (leftchar << 8) | data[i];
+                        leftbits += 8;
+                        while (leftbits >= 6) {
+                            var curr = (leftchar >> (leftbits - 6)) & 0x3f;
+                            leftbits -= 6;
+                            ret += BASE[curr];
+                        }
+                    }
+                    if (leftbits == 2) {
+                        ret += BASE[(leftchar & 3) << 4];
+                        ret += PAD + PAD;
+                    } else if (leftbits == 4) {
+                        ret += BASE[(leftchar & 0xf) << 2];
+                        ret += PAD;
+                    }
+                    return ret;
+                }
+
+                var audio_url = 'data:audio/x-wav;base64,' + encode64(event.data);
+
+                if (callback) {
+                    callback(audio_url);
+                } else {
+                    var audio = document.createElement('audio');
+                    audio.onended = function() {
+                        if (onSpeakingEnd) onSpeakingEnd();
+                    };
+                    audio.src = audio_url;
+                    audio.play();
+                }
+            };
+
+            if (args.onSpeakingEnd) delete args.onSpeakingEnd;
+            if (args.callback) delete args.callback;
+
+            speakWorker.postMessage({ text: text, args: args });
+        }
+    };
+}
